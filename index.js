2487e22d22fe8af3d568fe14bf4dd7466caf33be
Ivan Nor <ivan.norik@gmail.com>
2024-09-25 16:55:12 +0300

    fix header

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-var button = document.querySelector('header nav button')
-button.addEventListener('click', () => console.log('click'))
+const dropdownOpenButton = document.querySelector('.header-burger-button')
+const dropdownCloseButton = document.querySelector('.cross-button')
+const dropdownElement = document.querySelector('.dropdown')
+
+dropdownElement.addEventListener('blur', (e) => {
+  dropdownElement.classList.add('visually-hidden')
+  dropdownOpenButton.classList.toggle('visually-hidden')
+})
+
+dropdownOpenButton.addEventListener('click', (e) => {
+  dropdownElement.classList.toggle('visually-hidden')
+  dropdownElement.focus()
+  dropdownOpenButton.classList.add('visually-hidden')
+})
+
+
+dropdownCloseButton.addEventListener('click', (e) => {
+  dropdownElement.classList.add('visually-hidden')
+  dropdownOpenButton.classList.toggle('visually-hidden')
+})
+
