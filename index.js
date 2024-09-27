0a291d2f91ecb89725485ce160e481bebed2e141
Ivan Nor <ivan.norik@gmail.com>
2024-09-27 18:32:05 +0300

    add tab-switcher  in services section

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// выпадающее меню хедера
 const dropdownOpenButton = document.querySelector('.header-burger-button')
 const dropdownCloseButton = document.querySelector('.cross-button')
 const dropdownElement = document.querySelector('.dropdown')
@@ -13,9 +14,25 @@ dropdownOpenButton.addEventListener('click', (e) => {
   dropdownOpenButton.classList.add('visually-hidden')
 })
 
-
 dropdownCloseButton.addEventListener('click', (e) => {
   dropdownElement.classList.add('visually-hidden')
   dropdownOpenButton.classList.toggle('visually-hidden')
 })
+
+// переключение вкладок 
+const tabElements = document.querySelectorAll('#tabs button')
+console.log(tabElements)
+for (const tab of tabElements) {
+  console.log(tab)
+
+  tab.addEventListener('click', (e) => {
+    const activeTabElement = document.querySelector('button[tabindex="0"]')
+    const activeTabId = activeTabElement.id
+    console.log('click', e.target.id, activeTabId)
+    if (e.target.id !== activeTabElement)  {
+      activeTabElement.setAttribute('tabindex', '-1')
+      e.target.setAttribute('tabindex', '0')
+    }
+  })
+}
 
