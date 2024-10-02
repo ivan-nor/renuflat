1fff37bd0ec77c0fdbdff42a898ec6f4ea1e49dd
Ivan Nor <ivan.norik@gmail.com>
2024-10-02 10:57:51 +0300

    fix section projects & add swiper

--- swiper.js
+++ swiper.js
@@ -1,22 +1,13 @@
-
-var swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
   loop: true,
-
-  // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
+    clickable: true,
   },
-
-  // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
